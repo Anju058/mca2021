a2eb109944f162df993b0e77c20f13106a9da804
Anju058 <97436698+Anju058@users.noreply.github.com>
2022-03-07 00:34:25 +0530

    Delete ascen.py

--- ascen.py
+++
@@ -1,7 +0,0 @@
-y={'carl':40,'alan':2,'bob':1,'danny':3}
-l=list(y.items())
-print("dictionary",y)
-l.sort()
-print('ascending order is',dict(l))
-l.sort(reverse=True)
-print('descending order is',dict(l))
